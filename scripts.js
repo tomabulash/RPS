95a0fef07def1ce3b7500a7d6022f9b4b9c584bc
tomabulash <19751968+tomabulash@users.noreply.github.com>
2023-04-27 23:01:07 +0300

    first commit

---
+++ scripts.js
@@ -0,0 +1,107 @@
+function getComputerChoice() {
+
+    let computerChoice = Math.floor(Math.random() * 3);
+
+    if (computerChoice === 0) {
+        return 'rock';
+    } else if (computerChoice === 1) {
+        return 'paper';
+    } else {
+        return 'scissors';
+    }
+
+}
+
+
+
+function playRound(playerSelection, computerSelection) {
+
+    const player = playerSelection.toLowerCase();
+
+    if (player === 'rock' && computerSelection === 'paper') {
+        return 'You Lose! Paper beats Rock';
+    } else if (player === 'rock' && computerSelection === 'rock') {
+        return 'Tie! You both picked Rock';
+    } else if (player === 'rock' && computerSelection === 'scissors') {
+        return 'You Win! Rock beats Scissors';
+    } else if (player === 'paper' && computerSelection === 'rock') {
+        return 'You Win! Paper beats Rock';
+    } else if (player === 'paper' && computerSelection === 'paper') {
+        return 'Tie! You both picked Paper';
+    } else if (player === 'paper' && computerSelection === 'scissors') {
+        return 'You Lose! Scissors beats Paper';
+    } else if (player === 'scissors' && computerSelection === 'rock') {
+        return 'You Lose! Rock beats Scissors';
+    } else if (player === 'scissors' && computerSelection === 'scissors') {
+        return 'Tie! You both picked scissors';
+    } else if (player === 'scissors' && computerSelection === 'paper') {
+        return 'You Win! Scissors beats Paper';
+    }
+}
+
+
+function keepResult(roundResult) {
+    
+    let tie = roundResult.slice(0,3);
+    let win = roundResult.slice(0,7);
+
+    if (tie === 'Tie') {
+        return 1;
+    } else if (win === 'You Win') {
+        return 2;    
+    } else {
+        return 0;
+    }   
+}
+
+
+function getTotalWinner(playerTotal,computerTotal) {
+
+    if (playerTotal > computerTotal) {
+        console.log('You Win after 5 rounds!');
+    } else if (playerTotal < computerTotal) {
+        console.log('Computer wins after 5 rounds!');
+    } else {
+        console.log('Tie after 5 rounds!');
+    }
+
+}
+
+
+function game() {
+    
+    let playerTotal = 0;
+    let computerTotal = 0;
+
+    for (let i = 0; i < 5; i++) {
+        
+        console.log('Round ' + (i+1) + ' --------------');
+
+        let computerChoice = getComputerChoice();
+        let playerSelection = prompt('Enter Paper, Rock or Scissors');
+        let roundResult = '';
+
+
+        console.log('Computer selected: ' + computerChoice);
+        console.log('You selected: ' + playerSelection.toLowerCase());
+
+        roundResult = playRound(playerSelection, computerChoice);
+        console.log(roundResult);
+        
+        if ((keepResult(roundResult)) == 2) {
+            playerTotal++;
+        } else if((keepResult(roundResult)) == 0) {
+            computerTotal++;
+        } else {
+            computerTotal++;
+            playerTotal++;
+        }
+    }
+
+    console.log('Your total score: ' + playerTotal);
+    console.log('Computer total score: ' + computerTotal);
+    getTotalWinner(playerTotal,computerTotal);
+
+}
+
+game();
